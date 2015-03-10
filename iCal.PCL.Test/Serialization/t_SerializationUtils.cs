f019f52959c1c1d65a20e8238bc4d2c3958bccb5
Gordon Watts <gordon@gordonandpaula.net>
2015-03-10 19:03:12 +0100

    Add look into internals so we can do whitebox testing

---
+++ iCal.PCL.Test/Serialization/t_SerializationUtils.cs
@@ -0,0 +1,21 @@
+﻿using iCal.PCL.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace iCal.PCL.Test.Serialization
+{
+    [TestClass]
+    public class t_SerializationUtils
+    {
+        // Internals are only visible in the debug version
+#if DEBUG
+        [TestMethod]
+        public void BlankLinesRemoved()
+        {
+            var lines = TestUtils.FromParameterList("", "", "");
+            var r = lines.UnfoldLines().ToArray();
+            Assert.AreEqual(0, r.Length);
+        }
+#endif
+    }
+}
